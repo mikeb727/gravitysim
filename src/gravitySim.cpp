c351b13f410f581c365def1da8a3e0e0bfa65826
mikeb <44451590+mikeb727@users.noreply.github.com>
2023-05-09 17:55:37 -0400

    overhaul sim to use OpenGL-enabled mbgfx

--- src/gravitySim.cpp
+++ src/gravitySim.cpp
@@ -6,45 +6,57 @@
     can grab and throw objects by holding down
     the left mouse button and moving the mouse. */
 
-#include <SDL2/SDL_events.h>
-#include <SDL2/SDL_timer.h>
 #include <iostream>
 #include <map>
 
 #include "2DPhysEnv.h"
 #include "control.h"
 #include <argparse/argparse.hpp>
-#include <mb-libs/colors.h>
 #include <mb-libs/mbgfx.h>
-
-using std::cerr;
+#include <sstream>
 
 typedef std::map<std::string, Control> CtrlSet;
-typedef std::map<int, GraphicsTools::ColorRgba> ObjColorMap;
 typedef std::map<int, GraphicsTools::RenderObject> ObjMap;
 
 /* The dimensions of the simulation window. */
 const int ENV_WIDTH = 1024;
 const int ENV_HEIGHT = 768;
 
-/* The scale of the simulation ([pixelsPerMeter] pixels = 1 meter). */
+/* The scale of the simulation, in pixels per meter. */
 const double pixelsPerMeter = 120;
 
 /* The speed of the simulation, in frames per second. */
 const int framesPerSecond = 60;
 
-void drawSim(GraphicsTools::Window *win, Environment *env, ObjMap &ocm) {
+// previous cursor positions (for dragging velocity calculation)
+double cursorPosPrevX, cursorPosPrevY;
+
+void drawSim(GraphicsTools::Window *win) {
+  Environment *env = static_cast<Environment *>(win->userPointer("env"));
+  ObjMap *ocm = static_cast<ObjMap *>(win->userPointer("objmap"));
+
+  CtrlSet *ctrls = static_cast<CtrlSet *>(win->userPointer("ctrlset"));
+  GraphicsTools::Font *font =
+      static_cast<GraphicsTools::Font *>(win->userPointer("font"));
+
   // update renderobject positions
   for (auto &obj : env->objs()) {
     float drawPosX = obj.second.bbox()->pos().x() - (win->width() * 0.5f);
     float drawPosY =
-        (win->height() - obj.second.bbox()->pos().y()) - (win->height() *
-        0.5f);
-    ocm.at(obj.first).setPos(glm::vec3(drawPosX, drawPosY, 0.0f));
+        (win->height() - obj.second.bbox()->pos().y()) - (win->height() * 0.5f);
+    ocm->at(obj.first).setPos(glm::vec3(drawPosX, drawPosY, 0.0f));
   }
+
+  std::ostringstream ctrlStatus;
+  ctrlStatus << "radius " << ctrls->at("radius").getValue() << "\nvelocity "
+             << ctrls->at("velx").getValue() << " "
+             << ctrls->at("vely").getValue() << "\nelasticity "
+             << ctrls->at("elast").getValue();
   // draw all objs
   win->clear();
   win->activeScene()->render();
+  win->activeScene()->drawText2D(*font, ctrlStatus.str(),
+                                 GraphicsTools::Colors::Black, 50, 250, -1, 1);
   win->update();
 }
 
@@ -52,163 +64,167 @@ void updateSim(Environment *env) { env->update(); }
 
 void keyCallback(GLFWwindow *win, int key, int scancode, int action, int mods) {
   GraphicsTools::Window *mbWin =
-      (GraphicsTools::Window *)glfwGetWindowUserPointer(win);
-  Environment *env = (Environment *)mbWin->userPointer("env");
+      static_cast<GraphicsTools::Window *>(glfwGetWindowUserPointer(win));
+  Environment *env = static_cast<Environment *>(mbWin->userPointer("env"));
+  ObjMap *om = static_cast<ObjMap *>(mbWin->userPointer("objmap"));
   // If the 'C' key is pressed, remove all objects
   if (key == GLFW_KEY_C && action == GLFW_PRESS) {
+    for (auto &obj : env->objs()) {
+      mbWin->activeScene()->removeRenderObject(obj.first);
+    }
     env->clearObjs();
   }
   // If the space bar is pressed, pause the environment
   if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) {
     env->togglePause();
   }
+  // If the escape key is pressed, signal to quit simulation
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    mbWin->setShouldClose(1);
+  }
 }
 
-bool handleInput(GraphicsTools::Window *win, Environment *env, CtrlSet &ctrls,
-                 ObjColorMap &ocm) {
-  GraphicsTools::Scene *sc = win->activeScene();
-  SDL_Event event;
-  // If the escape key is pressed, signal to quit simulation
-  if (glfwGetKey(win->glfwWindow(), GLFW_KEY_ESCAPE)) {
-    win->setShouldClose(1);
-    cerr << "Escape key pressed\n";
+void mouseButtonCallback(GLFWwindow *win, int button, int action, int mods) {
+  GraphicsTools::Window *mbWin =
+      static_cast<GraphicsTools::Window *>(glfwGetWindowUserPointer(win));
+  Environment *env = static_cast<Environment *>(mbWin->userPointer("env"));
+  CtrlSet *ctrls = static_cast<CtrlSet *>(mbWin->userPointer("ctrlset"));
+  ObjMap *om = static_cast<ObjMap *>(mbWin->userPointer("objmap"));
+  ShaderProgram *shader =
+      static_cast<ShaderProgram *>(mbWin->userPointer("phongshader"));
+
+  double cursorPosX, cursorPosY;
+  bool objAtCursor;
+  glfwGetCursorPos(win, &cursorPosX, &cursorPosY);
+  Vec cursorPos(cursorPosX, cursorPosY);
+
+  if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+    for (auto &obj : env->objs()) {
+      if (obj.second.bbox()->containsPoint(cursorPos)) {
+        objAtCursor = true;
+        obj.second.setSelectState(true);
+        break;
+      }
+    }
   }
+  // left mouse release: create object if none present at cursor
+  if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
+    for (auto &obj : env->objs()) {
+      if (obj.second.bbox()->containsPoint(cursorPos)) {
+        objAtCursor = true;
+        obj.second.setSelectState(false);
+        break;
+      }
+    }
+    if (!objAtCursor) {
+      if (env->bbox()->containsBBox(
+              Circle(cursorPos, ctrls->at("radius").getValue()))) {
+        env->addObj(Object(
+            new Circle(cursorPos, ctrls->at("radius").getValue()), 1, cursorPos,
+            Vec(ctrls->at("velx").getValue(), ctrls->at("vely").getValue()),
+            ctrls->at("elast").getValue()));
+
+        GraphicsTools::Material mat = {GraphicsTools::randomColor(), NULL,
+                                       0.5*GraphicsTools::Colors::White, 4};
+        om->emplace(env->lastObjId(), GraphicsTools::RenderObject());
+        om->at(env->lastObjId()).setShader(shader);
+        om->at(env->lastObjId()).setMaterial(mat);
+        om->at(env->lastObjId())
+            .genSphere(ctrls->at("radius").getValue(), 16, 16);
+        om->at(env->lastObjId())
+            .setPos(glm::vec3(cursorPos.x() - (mbWin->width() / 2.0f),
+                              (mbWin->height() - cursorPos.y()) -
+                                  (mbWin->height() * 0.5f),
+                              0));
+        mbWin->activeScene()->addRenderObject(&om->at(env->lastObjId()));
+      }
+    }
+  }
+  if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS) {
+    for (auto &obj : env->objs()) {
+      if (obj.second.bbox()->containsPoint(cursorPos)) {
+        objAtCursor = true;
+        mbWin->activeScene()->removeRenderObject(obj.first);
+        env->removeObj(obj.first);
+        break;
+      }
+    }
+  }
+}
+
+void cursorPosCallback(GLFWwindow *win, double x, double y) {
+  GraphicsTools::Window *mbWin =
+      static_cast<GraphicsTools::Window *>(glfwGetWindowUserPointer(win));
+  Environment *env = static_cast<Environment *>(mbWin->userPointer("env"));
+  CtrlSet *ctrls = static_cast<CtrlSet *>(mbWin->userPointer("ctrlset"));
+  ObjMap *om = static_cast<ObjMap *>(mbWin->userPointer("objmap"));
+
+  double cursorPosX, cursorPosY;
+  bool objAtCursor;
+  glfwGetCursorPos(win, &cursorPosX, &cursorPosY);
+  Vec cursorPos(cursorPosX, cursorPosY);
+
+  if (glfwGetMouseButton(win, GLFW_MOUSE_BUTTON_LEFT)) {
+    for (auto &obj : env->objs()) {
+      if (obj.second.bbox()->containsPoint(cursorPos) &&
+          obj.second.selected()) {
+        objAtCursor = true;
+        obj.second.setPos(cursorPos);
+        obj.second.setVel(
+            75 * Vec(cursorPosX - cursorPosPrevX, cursorPosY - cursorPosPrevY));
+        break;
+      }
+    }
+  }
+
+  cursorPosPrevX = cursorPosX;
+  cursorPosPrevY = cursorPosY;
+}
+
+bool handleInput(GraphicsTools::Window *win) {
+  Environment *env = static_cast<Environment *>(win->userPointer("env"));
+  CtrlSet *ctrls = static_cast<CtrlSet *>(win->userPointer("ctrlset"));
+  GraphicsTools::Scene *sc = win->activeScene();
   // If the W key is pressed, increase elasticity of Objects created
   if (glfwGetKey(win->glfwWindow(), GLFW_KEY_W)) {
-    ctrls["radius"].changeValue(1.0);
-    cerr << "ctrl radius " << ctrls["radius"].getValue() << "\n";
+    ctrls->at("radius").changeValue(1.0);
+    std::cerr << "ctrl radius " << ctrls->at("radius").getValue() << "\n";
   }
   // If the Q key is pressed, decrease elasticity of Objects created
   if (glfwGetKey(win->glfwWindow(), GLFW_KEY_Q)) {
-    ctrls["radius"].changeValue(-1.0);
-    cerr << "ctrl radius " << ctrls["radius"].getValue() << "\n";
+    ctrls->at("radius").changeValue(-1.0);
+    std::cerr << "ctrl radius " << ctrls->at("radius").getValue() << "\n";
   }
   // If the S key is pressed, increase elasticity of Objects created
   if (glfwGetKey(win->glfwWindow(), GLFW_KEY_S)) {
-    ctrls["elast"].changeValue(0.01);
-    cerr << "ctrl elast " << ctrls["elast"].getValue() << "\n";
+    ctrls->at("elast").changeValue(0.01);
+    std::cerr << "ctrl elast " << ctrls->at("elast").getValue() << "\n";
   }
   // If the A key is pressed, decrease elasticity of Objects created
   if (glfwGetKey(win->glfwWindow(), GLFW_KEY_A)) {
-    ctrls["elast"].changeValue(-0.01);
-    cerr << "ctrl elast " << ctrls["elast"].getValue() << "\n";
+    ctrls->at("elast").changeValue(-0.01);
+    std::cerr << "ctrl elast " << ctrls->at("elast").getValue() << "\n";
   }
   //  velocity
   if (glfwGetKey(win->glfwWindow(), GLFW_KEY_LEFT)) {
-    ctrls["velx"].changeValue(-5);
-    cerr << "ctrl vel " << ctrls["velx"].getValue() << " "
-         << ctrls["vely"].getValue() << "\n";
+    ctrls->at("velx").changeValue(-5);
+    std::cerr << "ctrl vel " << ctrls->at("velx").getValue() << " "
+              << ctrls->at("vely").getValue() << "\n";
   }
   if (glfwGetKey(win->glfwWindow(), GLFW_KEY_RIGHT)) {
-    ctrls["velx"].changeValue(5);
-    cerr << "ctrl vel " << ctrls["velx"].getValue() << " "
-         << ctrls["vely"].getValue() << "\n";
+    ctrls->at("velx").changeValue(5);
+    std::cerr << "ctrl vel " << ctrls->at("velx").getValue() << " "
+              << ctrls->at("vely").getValue() << "\n";
   }
   if (glfwGetKey(win->glfwWindow(), GLFW_KEY_UP)) {
-    ctrls["vely"].changeValue(-5);
-    cerr << "ctrl vel " << ctrls["velx"].getValue() << " "
-         << ctrls["vely"].getValue() << "\n";
+    ctrls->at("vely").changeValue(-5);
+    std::cerr << "ctrl vel " << ctrls->at("velx").getValue() << " "
+              << ctrls->at("vely").getValue() << "\n";
   }
   if (glfwGetKey(win->glfwWindow(), GLFW_KEY_DOWN)) {
-    ctrls["vely"].changeValue(5);
-    cerr << "ctrl vel " << ctrls["velx"].getValue() << " "
-         << ctrls["vely"].getValue() << "\n";
-  }
-
-  if (event.type == SDL_MOUSEBUTTONDOWN) {
-    Vec mousePosition(event.button.x, event.button.y);
-    bool objAtLocation = false, ctrlAtLocation = false;
-    if (event.button.button == SDL_BUTTON_LEFT) {
-      // TODO reenable mouse interactions with controls
-      // for (const Control& ctrl: ctrls){
-      // 	if (ctrl.containsPoint(mousePosition)){
-      // 		ctrlAtLocation = true;
-      // 		ctrl.setFromPosition(mousePosition);
-      // 		break;
-      // 	}
-      // }
-      if (!ctrlAtLocation) {
-        for (auto &obj : env->objs()) {
-          if (obj.second.bbox()->containsPoint(mousePosition)) {
-            objAtLocation = true;
-            obj.second.setSelectState(true);
-            break;
-          }
-        }
-      }
-    }
-    if (event.button.button == SDL_BUTTON_RIGHT) {
-      for (auto &obj : env->objs()) {
-        if (obj.second.bbox()->containsPoint(mousePosition)) {
-          objAtLocation = true;
-          env->removeObj(obj.first);
-          break;
-        }
-      }
-    }
-  }
-
-  if (event.type == SDL_MOUSEBUTTONUP) {
-    Vec mousePosition(event.button.x, event.button.y);
-    bool objAtLocation = false, ctrlAtLocation = false;
-    if (event.button.button == SDL_BUTTON_LEFT) {
-      // for (const Control& ctrl: ctrls){
-      // 	if (ctrl.containsPoint(mousePosition)){
-      // 		ctrlAtLocation = true;
-      // 		break;
-      // 	}
-      // }
-      for (auto &obj : env->objs()) {
-        if (obj.second.bbox()->containsPoint(mousePosition)) {
-          objAtLocation = true;
-          obj.second.setSelectState(false);
-          break;
-        }
-      }
-      if (!objAtLocation && !ctrlAtLocation) {
-        if (env->bbox()->containsBBox(
-                Circle(mousePosition, ctrls["radius"].getValue()))) {
-          env->addObj(
-              Object(new Circle(mousePosition, ctrls["radius"].getValue()), 1,
-                     mousePosition,
-                     Vec(ctrls["velx"].getValue(), ctrls["vely"].getValue()),
-                     ctrls["elast"].getValue()));
-          ocm[env->lastObjId()] = GraphicsTools::randomColor();
-        }
-      }
-    }
-    if (event.button.button == SDL_BUTTON_RIGHT) {
-      // does nothing
-    }
-  }
-
-  if (event.type == SDL_MOUSEMOTION) {
-    Vec mousePosition(event.button.x, event.button.y);
-    bool objAtLocation = false, ctrlAtLocation = false;
-    if (SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(1)) {
-      // for (int i = 0; i < _ctrls.size(); i++){
-      // 	if (_ctrls[i].containsPoint(mousePosition)){
-      // 		ctrlAtLocation = true;
-      // 		_ctrls[i].setFromPosition(mousePosition);
-      // 		break;
-      // 	}
-      // }
-      if (!ctrlAtLocation) {
-        for (auto &obj : env->objs()) {
-          if (obj.second.bbox()->containsPoint(mousePosition) &&
-              obj.second.selected()) {
-            objAtLocation = true;
-            obj.second.setPos(mousePosition);
-            obj.second.setVel(
-                Vec((event.motion.xrel) *
-                        (pixelsPerMeter * pixelsPerMeter / framesPerSecond),
-                    (event.motion.yrel) *
-                        (pixelsPerMeter * pixelsPerMeter / framesPerSecond)));
-            break;
-          }
-        }
-      }
-    }
+    ctrls->at("vely").changeValue(5);
+    std::cerr << "ctrl vel " << ctrls->at("velx").getValue() << " "
+              << ctrls->at("vely").getValue() << "\n";
   }
 
   return false;
@@ -231,72 +247,56 @@ int main(int argc, char *argv[]) {
   std::vector<double> envGravity =
       argParser.get<std::vector<double>>("--gravity");
 
-  std::cerr << envDimensions[0] << " " << envDimensions[1] << "\n";
-
   Environment mainEnv(envDimensions[0], envDimensions[1],
                       Vec(envGravity[0], envGravity[1]) * pixelsPerMeter,
                       0.5 / framesPerSecond);
 
   CtrlSet mainCtrls;
-  mainCtrls["radius"] = Control("Radius", 15, 100, 30);
+  mainCtrls["radius"] = Control("Radius", 15, 100, 40);
   mainCtrls["elast"] = Control("Elasticity", 0, 1, 1);
-  mainCtrls["velx"] = Control("X velocity", -500, 500, 0);
-  mainCtrls["vely"] = Control("Y velocity", -500, 500, 0);
-
-  ObjColorMap mainOcm;
+  mainCtrls["velx"] = Control("X velocity", -1000, 1000, 0);
+  mainCtrls["vely"] = Control("Y velocity", -1000, 1000, 0);
 
   GraphicsTools::InitGraphics();
-  GraphicsTools::Window mainWindow("Gravity Sim", envDimensions[0], envDimensions[1]);
-  ShaderProgram phongShader("assets/phong_shadows_vs.glsl",
-                            "assets/phong_shadows_fs.glsl");
+  GraphicsTools::Window mainWindow("Gravity Sim", envDimensions[0],
+                                   envDimensions[1]);
   mainWindow.setClearColor({0.8, 0.7, 0.6, 1.0});
   ObjMap mainOM;
+
+  mainWindow.setUserPointer("env", &mainEnv);
+  mainWindow.setUserPointer("objmap", &mainOM);
+  mainWindow.setUserPointer("ctrlset", &mainCtrls);
+  ShaderProgram phongShader("assets/phong_shadows_vs.glsl",
+                            "assets/phong_shadows_fs.glsl");
+  mainWindow.setUserPointer("phongshader", &phongShader);
+  GraphicsTools::Font font1("assets/font.ttf", 20);
+  mainWindow.setUserPointer("font", &font1);
 
   GraphicsTools::Scene sc;
   mainWindow.attachScene(&sc);
 
   GraphicsTools::Camera cam1;
   cam1.setOrtho(mainWindow.width(), mainWindow.height());
-  cam1.setPos(glm::vec3(0.0f, 0.0f, 50.0f));
+  cam1.setPos(glm::vec3(0.0f, 0.0f, 200.0f));
   sc.addCamera(&cam1);
   sc.setActiveCamera(0);
 
-  GraphicsTools::DirectionalLight light2 = {glm::normalize(glm::vec3(0, 0, -1)),
-                                            {0.1, 0.1, 0.1, 1.0},
-                                            {1.0, 1.0, 1.0, 1.0},
-                                            {0.1, 0.1, 0.1, 1.0},
-                                            &phongShader};
+  GraphicsTools::DirectionalLight light2 = {
+      glm::normalize(glm::vec3(0, -0.2, -1)), 0.5 * GraphicsTools::Colors::White,
+      GraphicsTools::Colors::White, 0.1 * GraphicsTools::Colors::White,
+      &phongShader};
   sc.setDirLight(&light2);
 
   glfwSetKeyCallback(mainWindow.glfwWindow(), keyCallback);
-
-  for (int i = 0; i < 10; ++i) {
-    float posX = 200 + (70 * i);
-    float posY = 500 - (40 * i);
-    float rad = 30 + i;
-    GraphicsTools::Material mat = {GraphicsTools::randomColor(), NULL, GraphicsTools::Colors::White, 64};
-    mainEnv.addObj(Object(new Circle(Vec(posX, posY), rad), 1, Vec(posX, posY),
-                          Vec(20, 0), 1));
-    mainOM.emplace(mainEnv.lastObjId(), GraphicsTools::RenderObject());
-    mainOM.at(mainEnv.lastObjId()).setShader(&phongShader);
-    mainOM.at(mainEnv.lastObjId()).setMaterial(mat);
-    mainOM.at(mainEnv.lastObjId()).genSphere(rad, 8, 8);
-    mainOM.at(mainEnv.lastObjId())
-        .setPos(glm::vec3(
-            posX - (mainWindow.width() / 2.0f),
-            (mainWindow.height() - posY) - (mainWindow.height() * 0.5f), 0));
-    mainWindow.activeScene()->addRenderObject(&mainOM.at(mainEnv.lastObjId()));
-  }
+  glfwSetMouseButtonCallback(mainWindow.glfwWindow(), mouseButtonCallback);
+  glfwSetCursorPosCallback(mainWindow.glfwWindow(), cursorPosCallback);
 
   // While the program is running
   bool quit = false;
   while (!mainWindow.shouldClose()) {
-    // handleInput(&mainWindow, &mainEnv, mainCtrls, mainOcm);
-    if (mainEnv.time() % 60 == 0) {
-      mainEnv.print(std::cerr);
-    }
+    handleInput(&mainWindow);
     updateSim(&mainEnv);
-    drawSim(&mainWindow, &mainEnv, mainOM);
+    drawSim(&mainWindow);
   }
 
   GraphicsTools::CloseGraphics();
